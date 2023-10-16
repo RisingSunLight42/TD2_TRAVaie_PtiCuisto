094b1db23acef10364deb28e5ef59469d7885f18
Theolsr <136012627+Theolsr@users.noreply.github.com>
2023-10-16 15:37:45 +0200

    ✅ ADD pages, arborescence, and folders (#20)
    Co-authored-by: lesieur221 <lesieur22~1@etu.unicaen.fr>

---
+++ templates/all_recipes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nos recettes</title>
+</head>
+<body>
+    <div id="menuButton">
+        <a href="../index.php">Accueil</a>
+        <a href ="./recipe.php">Recette simple</a>
+    <div>
+</body>
+</html>
---
+++ templates/filter.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtres</title>
+</head>
+<body>
+    <div id="menuButton">
+        <a href="../index.php">Accueil</a>
+        <a href="./list.php">Liste</a>
+        <a href ="./recipe.php">Recette simple</a>
+    </div>
+</body>
+</html>
