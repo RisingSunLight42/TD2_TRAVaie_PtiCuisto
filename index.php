bbe7f11efb1f4f84ef54dd20d79283769f9322e6
lesieur221 <lesieur22~1@etu.unicaen.fr>
2023-10-16 15:27:58 +0200

    ✅ ADD pages and arborescence, and folders

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PtiCuistot - Accueil</title>
+</head>
+<body>
+    <div id="menuButton">
+        <a href="./index.php">Accueil</a>
+        <a href="./templates/all_recipes.php">Nos recettes</a>
+        <a href="./templates/filter.php">Filtrer</a>
+    </div>
+
+    
+</body>
+</html>
---
+++ templates/recipe.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recette</title>
+</head>
+<body>
+    <div id="menuButton">
+        <a href="../index.php">Accueil</a>
+        <a href="./all_recipes.php">Retour aux recettes</a>
+        <a href="./filter.php">Retour au filtre</a>
+    </div>
+</body>
+</html>
