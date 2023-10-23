7425ffe3737feb4fddfdb857e42afb2eff446294
lesieur221 <lesieur22~1@etu.unicaen.fr>
2023-10-23 08:31:28 +0200

    ✅ add recipe_creation feature, not finish

---
+++ recipe_creation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nouvelle Recette</title>
+</head>
+<body>
+    <form name="r_creation" method="post" action="recipe_creation.php">
+        <input type="submit" name="confirm" value="OK"/>
+    </form>
+</body>
+</html>
