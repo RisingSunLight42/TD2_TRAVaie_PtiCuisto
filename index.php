14f2d2adb5e4fc9394c99b3c27b34e7052e320b7
MaitreValentino <valentinsnith@hotmail.com>
2023-10-23 08:53:30 +0200

    ADD: Add a favicon

--- index.php
+++ index.php
@@ -4,15 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/bootstrap.rtl.min.css"/>
-    <link rel="stylesheet" href="styles/styles.css"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <link rel="icon" href="assets/images/Pticuisto_icon.ico"/>
     <title>PtiCuistot - Accueil</title>
 </head>
 <body>
-    <div id="menuButton">
-        <a href="./index.php">Accueil</a>
-        <a href="./templates/all_recipes.php">Nos recettes</a>
-        <a href="./templates/filter.php">Filtrer</a>
-    </div>
+
+    <main>
+
+        <div id="menuButton">
+            <a href="./index.php">Accueil</a>
+            <a href="./templates/all_recipes.php">Nos recettes</a>
+            <a href="./templates/filter.php">Filtrer</a>
+        </div>
+
+    </main>    
 
     
 </body>
