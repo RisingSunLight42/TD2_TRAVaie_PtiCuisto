2448b92610e639488226fcc3b9dbb6ddcc1b9796
Arthur <arthur.delcourt@etu.unicaen.fr>
2023-10-23 08:44:42 +0200

    Add compte.php

---
+++ templates/compte.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PtiCuistot - Compte</title>
+</head>
+<body>
+    <div id="menuButton">
+        <a href="/index.php">Accueil</a>
+        <a href="/templates/all_recipes.php">Nos recettes</a>
+        <a href="/templates/filter.php">Filtrer</a>
+    </div>
+    <div>
+        <button>Creation</button>
+        <button>Modification</button>
+        <button>Suppression</button>
+    </div>
+    
+</body>
+</html>
