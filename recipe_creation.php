18506f1a2a4f271581576e8cbed66359062ce52b
lesieur221 <lesieur22~1@etu.unicaen.fr>
2023-10-23 08:48:49 +0200

    📝 Form recipe

--- recipe_creation.php
+++ recipe_creation.php
@@ -6,7 +6,13 @@
     <title>Nouvelle Recette</title>
 </head>
 <body>
+    <p>Titre</p>
     <form name="r_creation" method="post" action="recipe_creation.php">
+        Entrez le titre de la recette : <input type="text" name="re_title"/> 
+        Entrez le contenu de votre recette : <input type="text"  name="re_desc"/>
+        
+        
+
         <input type="submit" name="confirm" value="OK"/>
     </form>
 </body>
