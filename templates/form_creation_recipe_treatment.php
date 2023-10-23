5f257da802f7c6e3338787c2b27d3a901dd74589
lesieur221 <lesieur22~1@etu.unicaen.fr>
2023-10-23 17:35:50 +0200

    📝 Update PDO without logins

--- templates/form_creation_recipe_treatment.php
+++ templates/form_creation_recipe_treatment.php
@@ -1,4 +1,6 @@
 <?php
+$bdd = new PDO('mysql:host=https://dev-lesieur221.users.info.unicaen.fr/;dbname=lesieur221_1;charset=utf8', '', ''); 
+
 if(isset($_POST['re_title']) && isset($_POST['re_desc']) && isset($_POST['re_resume']) && isset($_POST['re_cat'])) {
     $title = $_POST['re_title'];
     $desc = $_POST['re_desc'];
@@ -6,5 +8,5 @@ if(isset($_POST['re_title']) && isset($_POST['re_desc']) && isset($_POST['re_res
     $categorize = $_POST['re_cat'];
 }
 $sql= "insert into ptic_recipes(reci_title, reci_content, reci_resume, rtype_id, reci_creation_date, reci_edit_date,users_id) values ('".$title."'".","."'".$desc."'".","."'".$resume."'".",".$categorize.", sysdate(), sysdate(),1)";
-echo $sql;
+LireDonneesPDOPreparee($sql,$tab);
 ?>
