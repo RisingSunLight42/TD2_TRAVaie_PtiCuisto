c809bef2e698dfc185be54d68a0bbf5a18c14963
Arthur <arthur.delcourt@etu.unicaen.fr>
2023-10-26 18:58:16 +0200

    add something ?

--- templates/all_recipes.php
+++ templates/all_recipes.php
@@ -6,37 +6,37 @@
     <title>Nos recettes</title>
 </head>
 <body>
+    
+
     <div id="menuButton">
         <a href="../index.php">Accueil</a>
         <a href ="./recipe.php">Recette simple</a>
     <div>
         <?php
-
         
         $nombre = 10;
 
-        require_once("connexion.php");
-        include_once("pdo_agile.php");
+            require_once("connexion.php");
+            include_once("pdo_agile.php");
+            $requete = "select reci_title, reci_resume, rtype_title, reci_image 
+            from ptic_recipes 
+            inner join ptic_recipes_type on ptic_recipes.rtype_id = ptic_recipes_type.rtype_id
+            where reci_id <=".$nombre;
+
+            LireDonneesPDO1($bdd,$requete, $tab);
 
         
+            for ($i= 0; $i < count($tab); $i++){
+                echo "<div>";
+                echo '<img src='.$tab[$i]['reci_image'].'alt="image de recetee"/>';
+                echo "<h1>".$tab[$i]['reci_title']."</h1>";
+                echo "<h2>".$tab[$i]['rtype_title']."</h2>";
+                echo "<p>".$tab[$i]['reci_resume']."<p>";
+                echo "<div>";           
+            }
+            echo "<button>afficher plus</button>";
 
-        $requete = "select reci_title, reci_resume, rtype_title, reci_image 
-        from ptic_recipes 
-        inner join ptic_recipes_type on ptic_recipes.rtype_id = ptic_recipes_type.rtype_id
-        where reci_id <=".$nombre;
 
-        LireDonneesPDO1($bdd,$requete, $tab);
-
-
-        for ($i= 0; $i < count($tab); $i++){
-            echo "<div>";
-            echo '<img src='.$tab[$i]['reci_image'].'alt="image de recetee"/>';
-            echo "<h1>".$tab[$i]['reci_title']."</h1>";
-            echo "<h2>".$tab[$i]['rtype_title']."</h2>";
-            echo "<p>".$tab[$i]['reci_resume']."<p>";
-            echo "<div>";           
-        }
-        echo "<button>afficher plus</button>";
         ?>
 </body>
 </html>
