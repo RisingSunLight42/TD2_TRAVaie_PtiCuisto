d8695358b1b52eaab17e1890e699c145dbf00dff
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-11-07 02:02:35 +0100

    ➕ Add connexionView and edit the accountView to be accurate

---
+++ assets/php/views/connectionView.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PtiCuistot - Connexion</title>
+</head>
+<body>
+    <div id="menuButton">
+        <a href="index.php?action=accueil">Accueil</a>
+        <a href="index.php?action=allRecipes">Nos recettes</a>
+        <a href="index.php?action=filter">Filtrer</a>
+    </div>
+    <form action="index.php?action=connectionForm" method="post">
+        <label for="username">Nom d'utilisateur</label>
+        <input type="text" name="username" id="username" placeholder="Jean-Marc666">
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" placeholder="******">
+        <input type="submit" value="Connexion">
+    </form>
+</body>
+</html>
