fb39fc031fbae5142bc28dca5f09d469b4ebb0f1
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-11-11 18:18:37 +0100

    ➕ Add forms in filterView

--- assets/php/views/filterView.php
+++ assets/php/views/filterView.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script
+            src="https://kit.fontawesome.com/908c130cac.js"
+            crossorigin="anonymous"
+        ></script>
     <title>Filtres</title>
 </head>
 <body>
@@ -10,5 +15,34 @@
         <a href="index.php">Accueil</a>
         <a href ="index.php?action=recipe">Recette simple</a>
     </div>
+    <form method="post" action="index.php?action=filterRecipeName">
+        <label for="title">Titre :</label>
+        <input type="text" name="title" id="reci_title">
+        <input type="submit" value="Filtrer">
+    </form>
+    <form method="post" action="index.php?action=filterRecipeCategory">
+        <label for="re_cat">Catégorie :</label>
+        <input type="radio" name="re_cat" value="ENTREE"/>
+        <label>Entrée</label>
+        <input type="radio" name="re_cat" value="PLAT"/>
+        <label>Plat</label>
+        <input type="radio" name="re_cat" value="DESSERT"/>
+        <label>Dessert</label>
+        <input type="radio" name="re_cat" value="APERITIF"/>
+        <label>Aperitif</label>
+        <input type="radio" name="re_cat" value="BOISSON"/>
+        <label>Boisson</label> <br/>
+        <input type="submit" value="Filtrer">
+    </form>
+    <form method="post" action="index.php?action=filterRecipeIngredients">
+        <label for="ingredient">Ingrédients :</label>
+        <div class="autocomplete" style="width:300px;">
+            <input id="ingredient" type="text" name="ingredient" placeholder="Ingrédient">
+        </div>
+        <input type="hidden" id="nbIngredients" name="nbIngredients" value="0"/>
+        <input type="submit" value="Filtrer">
+    </form>
+    <script src="assets\js\autocompletion.js"></script>
+    <script src="assets\js\allIngredients.js"></script>
 </body>
 </html>
