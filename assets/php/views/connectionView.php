c0125d13b21d44f7ba9840261d0b1e5d4c120cd5
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-11-12 12:58:56 +0100

    🐛 fix footer positionning

--- assets/php/views/connectionView.php
+++ assets/php/views/connectionView.php
@@ -8,22 +8,25 @@
     ?>
     <title>PtiCuistot - Connexion</title>
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
     <?php 
         require ("header.php");
     ?>
-    <?php
-    echo $content;
-    ?>
-    <h1 id="welcomeBackMessage">Content de vous revoir !</h1>
-    <section id="formContent">
-        <form id="connectionContent" action="index.php?action=connectionForm" method="post">
-            <label for="username">Nom d'utilisateur</label>
-            <input type="text" name="username" id="username" placeholder="Super Jean-mi" required>
-            <label for="password">Mot de passe</label>
-            <input type="password" name="password" id="password" placeholder="Votre mot de passe" required>
-            <input type="submit" id="connectionButton" value="Connexion">
-        </form>
-    </section>
+    <main>
+        <?php
+        echo $content;
+        ?>
+        <h1 id="welcomeBackMessage">Content de vous revoir !</h1>
+        <section id="formContent">
+            <form id="connectionContent" action="index.php?action=connectionForm" method="post">
+                <label for="username">Nom d'utilisateur</label>
+                <input type="text" name="username" id="username" placeholder="Super Jean-mi" required>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" placeholder="Votre mot de passe" required>
+                <input type="submit" id="connectionButton" value="Connexion">
+            </form>
+        </section>
+    </main>
+    <?php require("footer.php") ?>
 </body>
 </html>
--- assets/php/views/recipeCreationView.php
+++ assets/php/views/recipeCreationView.php
@@ -6,47 +6,42 @@
     <?php 
         require ("head.php");
     ?>
-  <title>Nouvelle Recette</title>
-    <script
-            src="https://kit.fontawesome.com/908c130cac.js"
-            crossorigin="anonymous"
-        ></script>
     <title>Nouvelle Recette</title>
 </head>
 <body>
     <?php 
         require ("header.php");
     ?>
-    <h1>Nouvelle recette</h1>
-    <?php
-    echo $content;
-    ?>
-    <form id="re_form" name="r_creation" method="post" action="index.php?action=recipeCreationHandling">
-        <label for="re_title">Entrez le titre de la recette* :</label><input type="text" name="re_title" required/>
-        <label for="re_desc">Entrez le contenu de votre recette* :</label><textarea name="re_desc" rows="10" required></textarea><br>
-        <label for="re_resume">Entrez un résumé de votre recette* :</label><textarea name="re_resume" rows="5" required></textarea><br>
-        <label for="re_img">Entrez un lien vers l'image de votre recette :</label><input type ="text" name="re_img"/>
-        <label for="ingredient">Ajoutez vos ingrédients* :</label>
-        <div class="autocomplete" style="width:300px;">
-            <input id="ingredient" type="text" name="ingredient" placeholder="Ingrédient">
-        </div>
-        <br>
-        <label for="re_cat">Quel est la catégorie de votre recette ?* </label>
-        <input type="radio" name="re_cat" value="ENTREE"/>
-        <label>Entrée</label>
-        <input type="radio" name="re_cat" value="PLAT"/>
-        <label>Plat</label>
-        <input type="radio" name="re_cat" value="DESSERT"/>
-        <label>Dessert</label>
-        <input type="radio" name="re_cat" value="APERITIF"/>
-        <label>Aperitif</label>
-        <input type="radio" name="re_cat" value="BOISSON"/>
-        <label>Boisson</label> <br/>
-        <input type="submit" name="confirm" value="OK"/> 
-        <input type="hidden" id="nbIngredients" name="nbIngredients" value="0"/>
-    </form>
-
-    
+    <main>
+        <h1>Nouvelle recette</h1>
+        <?php
+        echo $content;
+        ?>
+        <form id="re_form" name="r_creation" method="post" action="index.php?action=recipeCreationHandling">
+            <label for="re_title">Entrez le titre de la recette* :</label><input type="text" name="re_title" required/>
+            <label for="re_desc">Entrez le contenu de votre recette* :</label><textarea name="re_desc" rows="10" required></textarea><br>
+            <label for="re_resume">Entrez un résumé de votre recette* :</label><textarea name="re_resume" rows="5" required></textarea><br>
+            <label for="re_img">Entrez un lien vers l'image de votre recette :</label><input type ="text" name="re_img"/>
+            <label for="ingredient">Ajoutez vos ingrédients* :</label>
+            <div class="autocomplete" style="width:300px;">
+                <input id="ingredient" type="text" name="ingredient" placeholder="Ingrédient">
+            </div>
+            <br>
+            <label for="re_cat">Quel est la catégorie de votre recette ?* </label>
+            <input type="radio" name="re_cat" value="ENTREE"/>
+            <label>Entrée</label>
+            <input type="radio" name="re_cat" value="PLAT"/>
+            <label>Plat</label>
+            <input type="radio" name="re_cat" value="DESSERT"/>
+            <label>Dessert</label>
+            <input type="radio" name="re_cat" value="APERITIF"/>
+            <label>Aperitif</label>
+            <input type="radio" name="re_cat" value="BOISSON"/>
+            <label>Boisson</label> <br/>
+            <input type="submit" name="confirm" value="OK"/> 
+            <input type="hidden" id="nbIngredients" name="nbIngredients" value="0"/>
+        </form>
+    </main>
     <?php 
         require ("footer.php");
     ?>
